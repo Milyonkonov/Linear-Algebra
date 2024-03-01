4bb37c0effa338906b33d557a1d04a1aa64277e9
Milyonkonov <emil.axel.karlsson@gmail.com>
2024-03-01 18:22:46 +0100

    All labs finally done.

--- Lab5/Uppgift2.py
+++ Lab5/Uppgift2.py
@@ -15,37 +15,35 @@ def RayleighQuotient (B, b):
 def PowerIteration(B, p):
     v = np.random.rand(B.shape[0], 1)   # v0 = random vector
     v /= LA.norm(v)                     # v0 måste också vara normaliserad
-    
-    newRayleighQuotient = RayleighQuotient(B, v) # Deklarera variabel innan loop.
-    newRayleighQuotient = 7272 # TODO WTF
 
-    # Fortsätt loopen så länge rayleighkvoten inte är precis nog.
-    while (abs(newRayleighQuotient - RayleighQuotient(B, v)) >= 10**-p):
-        newRayleighQuotient = RayleighQuotient(B, v)
-        v = B @ v
+    while (True):
+        oldRayleighQuotient = RayleighQuotient(B, v) # Spara rayleighkvoten av iterationen före, för precisionsjämförning senare.
+        v = B @ v       
         v /= LA.norm(v)
+
+        # Avbryt loopen ifall rayleighkvoten är precis nog.
+        if (abs(oldRayleighQuotient - RayleighQuotient(B, v)) <= 10**-p):
+            break
     
     eigenValue = LA.norm(B@v) / LA.norm(v) # lambda * v = Bv => lambda = |Bv| / |v|
     return (eigenValue, v)
 
 # 2a)
 print("2a)")
-taskaB = np.array([ [9, 5],
+B = np.array([ [9, 5],
                     [1, 5]])
 
-ourEig = PowerIteration(taskaB, 300)
-numpyEig = LA.eig(taskaB)
-print(ourEig[1])
-print(numpyEig.eigenvectors)
-print("Vårt beräknade egenvärde: {0} | numpys egenvärde: {1} \n".format(
-    ourEig[0], numpyEig.eigenvalues[0]))
+ourEig = PowerIteration(B, 10)
+numpyEig = LA.eig(B)
+
+print("Vårt beräknade egenvärde: {0} | numpys egenvärde: {1} \n".format(ourEig[0], numpyEig.eigenvalues[0]))
 
 # 2b)
 print("2b)")
-taskbA = np.random.rand(500, 500)
-taskbB = taskbA + taskbA.transpose()
+A = np.random.rand(500, 500)
+B = A + A.transpose()
 
-ourEig = PowerIteration(taskbB, 100)
+ourEig = PowerIteration(B, 10)
 print("Vårt beräknade egenvärde: {0}\n".format(ourEig[0]))
 
-print(LA.norm((taskbA @ ourEig[1]) - ourEig[0] * ourEig[1]))
+print("Av - (lambda)v = {0}".format(LA.norm((A @ ourEig[1]) - ourEig[0] * ourEig[1])))
